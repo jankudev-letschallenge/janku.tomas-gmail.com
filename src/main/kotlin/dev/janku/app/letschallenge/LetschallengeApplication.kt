fe42f5d58f7bdbddc00c288ed258c3905a07559b
janku.dev <janku.tomas@gmail.com>
2020-03-27 22:04:02 +0100

    Initial commit

---
+++ src/main/kotlin/dev/janku/app/letschallenge/LetschallengeApplication.kt
@@ -0,0 +1,11 @@
+package dev.janku.app.letschallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetschallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetschallengeApplication>(*args)
+}
---
+++ src/test/kotlin/dev/janku/app/letschallenge/LetschallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.janku.app.letschallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LetschallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
